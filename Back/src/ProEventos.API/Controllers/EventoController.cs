b9806e337e5d62b59501506119fc54699a2cbb5a
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2023-06-27 21:56:43 -0300

    27/06/2023 -> Instalado EF Core 5.0.2 na aplicação; -> Configurando connection string; -> Realizando a Migração dos dados para DB SQLite; -> Alterando e configurando context da controller Evento;

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,42 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        private readonly DataContext _context;
 
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento(){
-                    EventoID = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Caxias do Sul",
-                    Lote = "1º lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemURL = "photo.png"
-                },
-            new Evento(){
-                EventoID = 2,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Caxias do Sul",
-                Lote = "1º lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "photo.png"
-            }
-        };
-
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            this._context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public Evento Get(int id)
         {
-            return _evento.FirstOrDefault(s => s.EventoID == id);
+            return _context.Eventos.FirstOrDefault(s => s.EventoID == id);
         }
 
         [HttpPost]
